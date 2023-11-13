6db3ba7e1eaf0e85b8eb29425aff73738fb5e43d
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2023-11-13 12:10:20 +0100

    Update zad3.py

--- 06.11/zad3.py
+++ 06.11/zad3.py
@@ -1,6 +1,24 @@
 import random
-
 lista = []
-def generuj(n,min,max):
-    for i in range(o,n):
-        lista.append(random.randint(min,max))
+def generuj(n, min, max):
+    for i in range(0, n):
+        lista.append(random.randint(min, max))
+
+
+
+def ile_ujemnych(list):
+    u = 0
+    for i in list:
+        if i < 0:
+            u += 1
+    return u
+
+def main():
+    a = int(input('Podaj długość listy: '))
+    b = int(input('Podaj element minimalny: '))
+    c = int(input('Podaj element maksymalny: '))
+
+    generuj(a,b,c)
+    print(ile_ujemnych(lista))
+
+main()
