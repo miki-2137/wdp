e62fc4e7ce409a659752bbc7b84529a77a4372be
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2023-11-20 09:32:26 +0100

    file added

---
+++ 13.11/remove.py
@@ -0,0 +1,32 @@
+napis = "abrakadabra"
+usuwany = "ab"
+
+
+def remove(napis, usuwany):
+    for i in range(len(napis)):
+        if napis[i] == usuwany[0]:
+            d = i + len(usuwany)
+            us = napis[i:d]
+            if usuwany == us:
+                return napis[:i] + napis[d:]
+    return napis
+
+def remove_all(napis, usuwany):
+    wynik = napis
+    for i in range(len(napis)):
+        if napis[i] == usuwany[0]:
+            d = i + len(usuwany)
+            us = napis[i:d]
+            if usuwany == us:
+                wynik = napis[:i] + napis[d:]
+                for j in range(len(wynik)):
+                    if remove(wynik, usuwany) != wynik:
+                        wynik = remove(wynik,usuwany)
+    return wynik
+
+
+def main():
+    print(remove(napis,usuwany))
+    print(remove_all(napis,usuwany))
+
+main()
