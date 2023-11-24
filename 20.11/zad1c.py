fdf3b6f14664e364d64e8c9b028d431717b27b78
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2023-11-24 17:42:32 +0100

    Update zad1c.py

---
+++ 20.11/zad1c.py
@@ -0,0 +1,24 @@
+n = int(input('podaj liczbe: '))
+
+def dzielniki(n):
+    dz = []
+    for i in range(1,n+1):
+        if n%i == 0:
+            dz.append(i)
+    return dz
+    
+def czy_pierwsza(n):
+    if dzielniki(n) == [1,n]:
+        return True
+    else: return False
+    
+def mn_od(n):
+    mn = []
+    for i in dzielniki(n):
+        if czy_pierwsza(i) == True:
+            mn.append(i)
+    return mn
+    
+print(dzielniki(n))
+print(czy_pierwsza(n))
+print(mn_od(n))
