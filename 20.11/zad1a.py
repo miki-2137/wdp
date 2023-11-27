360aba214b9402ed8b940334c66b1c312a7bbe87
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2023-11-27 11:46:23 +0100

    Update zad1a.py

--- 20.11/zad1a.py
+++ 20.11/zad1a.py
@@ -12,7 +12,7 @@ def wzglednie(a, b):
 ---------------------------------------
 def wzglednie(a,b):
     x = 1
-    for i in range(1,a+1):
+    for i in range(1,a):
         if a%i == 0 and b%i == 0:
             x = i
     return x == 1
