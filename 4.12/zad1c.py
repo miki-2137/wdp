b95073336e1134ce6cbced91ae9c6b8000e55095
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2023-12-11 14:15:49 +0100

    Update zad1c.py

--- 4.12/zad1c.py
+++ 4.12/zad1c.py
@@ -4,18 +4,13 @@ zdanie = 'Ala ma kota.'
 def slownik(zdanie):
     d = {}
     for i in zdanie:
+        if not i.isalpha():
+            continue
+        i = i.lower()
         if i not in d:
             d[i]=1
         else:
             d[i]+=1
     return d
 
-def litery(slownik):
-    d = {}
-    for k in slownik:
-        k=k.lower()
-        if k.isalpha():
-            d[k]=slownik[k]
-    return d
-
-print(litery(slownik(zdanie)))
+print(slownik(zdanie))
