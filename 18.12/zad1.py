5e3f898e009b3d3b08e4ed23a5bf6cfaf9b79df1
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2024-01-02 13:48:03 +0100

    Update zad1.py

--- 18.12/zad1.py
+++ 18.12/zad1.py
@@ -1,20 +1,19 @@
-pwd = 'Password'
+pwd = 'Password '
 
 def slownik(string):
     d = {}
     for i in string:
         if not i.isalpha():
-            d[i] = 1
+            if not i.isspace():
+                d[i] = 1
         elif i.isupper():
             d[i] = 1
     return d
 
-print(slownik(pwd))
-
 def haslo(d):
-    if len(d)<2:
-        return 'chujowe haslo lmao l+ratio'
+    if len(d) < 2:
+        return False
     else:
-        return 'gituwa haslo szefie 8)))'
+        return True
 
 print(haslo(slownik(pwd)))
