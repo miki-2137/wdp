a3be87c7048d4e3f73bf44c9f42ae05a6e85f910
miki-2137 <92546137+miki-2137@users.noreply.github.com>
2024-01-02 14:06:51 +0100

    Update zad3.py

--- 4.12/zad3.py
+++ 4.12/zad3.py
@@ -1,14 +1,13 @@
-pwd = 'Pas@'
+pwd = 'Password@'
 
 def slownik(string):
     d = {}
-    if len(string)<8:
-        d[len]=0
-        return d
     for i in string:
         if not i.isalpha():
             if not i.isspace():
                 d[i] = 1
+            else:
+                return d
         elif i.isupper():
             d[i] = 1
     return d
